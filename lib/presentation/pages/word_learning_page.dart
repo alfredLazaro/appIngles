bd22d123435af74e096d6fc11f18f341e1a4f9fa
fred <alfredolazaro999@gmail.com>
2026-01-10 15:57:53 -0300

    refact(word_learning): import the correct move file.

--- lib/presentation/pages/word_learning_page.dart
+++ lib/presentation/pages/word_learning_page.dart
@@ -1,5 +1,5 @@
 import 'package:first_app/domain/entities/word_sumary.dart';
-import 'package:first_app/presentation/widgets/dialogs/combine_word_dialog.dart';
+import 'package:first_app/presentation/widgets/modals/combine_word_dialog.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:first_app/core/services/speech_to_text_service.dart';
