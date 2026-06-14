5ea14f7b24ddc164786ec43d6c24112900a31c9f
fred <alfredolazaro999@gmail.com>
2026-06-14 17:33:51 -0400

    arreglo unsplash

--- lib/data/datasources/remote/unsplash_service.dart
+++ lib/data/datasources/remote/unsplash_service.dart
@@ -4,30 +4,27 @@ import 'package:dio/dio.dart';
 class ImageService {
   static final ImageService _instance = ImageService._internal();
   factory ImageService() => _instance;
+
+  late final String _apiKey;
+  late final String _basUrl;
+  late final Dio _dio;
+
   ImageService._internal() {
-    _init();
-  }
-  String? _apiKey;
-  String? _basUrl;
-  late Dio _dio;
-  Future<void> _init() async {
-    await dotenv.load(fileName: 'assets/.env');
-    _apiKey = dotenv.env['KEY_UNS'];
-    _basUrl = dotenv.env['URL_UNS'];
-    if (_apiKey == null || _apiKey!.isEmpty) {
-      throw Exception("API_KEY no encontrada en .env");
+    _apiKey = dotenv.env['KEY_UNS'] ?? '';
+    if (_apiKey.isEmpty) {
+      throw Exception('KEY_UNS no encontrada en .env');
     }
 
-    if (_basUrl == null || _basUrl!.isEmpty) {
-      throw Exception("API_BASE_URL no encontrada en .env");
+    _basUrl = dotenv.env['URL_UNS'] ?? '';
+    if (_basUrl.isEmpty) {
+      throw Exception('URL_UNS no encontrada en .env');
     }
 
     _dio = Dio(
       BaseOptions(
-        baseUrl: _basUrl!,
+        baseUrl: _basUrl,
         connectTimeout: const Duration(seconds: 5),
-        receiveTimeout:
-            const Duration(seconds: 3),
+        receiveTimeout: const Duration(seconds: 3),
       ),
     );
   }
@@ -59,17 +56,19 @@ class ImageService {
           options: Options(headers: {'Authorization': 'Client-ID $_apiKey'}));
       if (response.data['results'] == null ||
           (response.data['results'] as List).isEmpty) {
-        throw Exception('No se encontraron imageners para "$nImg"');
+        throw Exception('No se encontraron imágenes para "$nImg"');
       }
       final List<Map<String, dynamic>> images =
-      (response.data['results'] as List).map((photo) {
+          (response.data['results'] as List).map((photo) {
         return {
           'id': photo['id'],
           'url': {
             'regular':
                 photo['urls']?['regular'] ?? 'https://default-image-url.com',
-            'small': photo['urls']?['small'] ?? 'https://default-image-url.com',
-            'thumb': photo['urls']?['thumb'] ?? 'https://default-image-url.com',
+            'small':
+                photo['urls']?['small'] ?? 'https://default-image-url.com',
+            'thumb':
+                photo['urls']?['thumb'] ?? 'https://default-image-url.com',
           },
           'user': {'name': photo['user']?['name'] ?? 'Autor desconocido'},
           'alt_description': 'Unsplash',
